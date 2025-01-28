81ba5dfabc5af74745d39e4b2b8f4dd017b257e0
gcronax <guillecampher15@gmail.com>
2025-01-28 19:51:05 +0100

    asdasd

--- src/HundirLaFlota.java
+++ src/HundirLaFlota.java
@@ -1,9 +1,25 @@
+import java.util.Arrays;
+
 public class HundirLaFlota {
     public static void main(String[] args) {
         Tablero.crearPartida();
 
         do{
-
+            Tablero.mostrarTablero();
+            int count=1;
+            System.out.println("_________________");
+            System.out.println("  A B C D E F G H");
+            for (int[] aux:Tablero.arrayTablero){
+                System.out.println((count++)+" "+Arrays.toString(aux)
+                        .replaceAll("0","~")
+                        .replaceAll("1","X")
+                        .replaceAll(",","")
+                        .replaceAll("]","")
+                        .replaceAll("\\[","")
+                        .replaceAll("2","~")
+                );
+            }
+            Tablero.mostrarEstadistica();
             Tablero.tiro(
                     InterfazUsuario.inputFila(),
             InterfazUsuario.inputColumna());
--- src/Tablero.java
+++ src/Tablero.java
@@ -9,36 +9,59 @@ public class Tablero {
     public static void crearPartida(){
         for (int i = 1; i <= 10; i++) {
             barcos.put(i,Arrays.asList(
-                    new Random().nextInt(10) + 1,
-                    new Random().nextInt(10) + 1) );
+                    new Random().nextInt(8) + 1,
+                    new Random().nextInt(8) + 1));
         }
     }
 
+    public static void mostrarTablero() {
+        int count=1;
 
+        int[][] auxarray=arrayTablero;
+
+        for ( List<Integer> valor : barcos.values()){
+            auxarray[valor.getFirst()-1][valor.getLast()-1]=2;
+        }
+        System.out.println("  A B C D E F G H");
+        for (int[] aux:auxarray){
+            System.out.println((count++)+" "+Arrays.toString(aux)
+                    .replaceAll("0","~")
+                    .replaceAll("1","X")
+                    .replaceAll(",","")
+                    .replaceAll("]","")
+                    .replaceAll("\\[","")
+                    .replaceAll("2","@")
+            );
+        }
+    }
 
 
     public static void tiro(int fila, int columna){
         Tablero.fila =fila;
         Tablero.columna=columna;
-        System.out.println(barcos);
+        //System.out.println(barcos);
         Integer aux=0;
         for (Integer clave : barcos.keySet()){
             if (barcos.get(clave).equals(Arrays.asList(fila, columna))){
-                System.out.println("barco encontrado");
+                System.out.println("barco undido");
                 aux=clave;
+                arrayTablero[fila-1][columna-1]=1;
+
             }
         }
         if (barcos.containsKey(aux)){
             barcos.remove(aux);
+        }else {
+            System.out.println("golazo");
         }
-        System.out.println(barcos);
 
-
-
-
+        //System.out.println(barcos);
 
     }
+    public static void mostrarEstadistica(){
+        System.out.println("has undido "+(10- barcos.size())+" barcos");
+    }
     public static boolean comprobarFinPartida(){
-        return false;
+        return barcos.isEmpty();
     }
 }
